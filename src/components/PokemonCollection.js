f0eb5328e23da8f2104a9a23391a8aa1e61fbfee
Ernest <ernestwinata@gmail.com>
2023-06-13 23:26:21 -0700

    creates myfile.md

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,12 +2,22 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemons }) {
   return (
     <Card.Group itemsPerRow={6}>
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
       <h1>Hello From Pokemon Collection</h1>
     </Card.Group>
   );
-}
+// }
 
+// function PokemonPage() {
+//   return (
+//     <Container>
+//       <PokemonCollection pokemons={pokemonData} />
+//     </Container>
+//   );
+}
 export default PokemonCollection;
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredPokemons = pokemonData.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
+  useEffect(() => {
+    fetch(`http://localhost:3001/pokemon`)
+    .then(response => response.json())
+    .then(data => setPokemonData(data));
+  }, []);
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={filteredPokemons} />
     </Container>
   );
 }
